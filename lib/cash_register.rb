757fa44088e25af17b42060abd3309abc690c232
ZacharyDagnall <zachary.dagnall@gmail.com>
2021-01-12 13:19:20 -0500

    commit tree aebb0632bbd5b06983c942b8a26efc4d61f9ddef

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,36 @@
+require 'pry'
 
+class CashRegister
+    attr_accessor :total, :prev_total, :discount, :items #employee discount
+    
+    def initialize(discount=0)
+        @total = 0
+        @prev_total = 0
+        @discount = discount
+        @items = []
+    end
+
+    def add_item(title,price,quantity=1)
+        @prev_total = @total
+        @total+=price*quantity
+        for i in 1..quantity
+            @items.push(title)
+        end
+    end
+
+    def apply_discount
+       if discount==0
+        return "There is no discount to apply."
+       else
+        @total = (@total*(1.0-@discount/100.0)).round(2)
+        return "After the discount, the total comes to $#{@total.round(0)}."
+       end
+    end
+
+    def void_last_transaction
+        @total = @prev_total
+    end
+
+
+
+end
